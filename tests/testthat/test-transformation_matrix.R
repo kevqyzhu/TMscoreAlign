cdab5029b1d719032ccffd510e35a220dad089a5
kevqyzhu <kevqyzhu@gmail.com>
2023-11-14 19:20:14 -0500

    feat: added test cases for get_matrix function

--- tests/testthat/test-transformation_matrix.R
+++ tests/testthat/test-transformation_matrix.R
@@ -25,3 +25,44 @@ test_that("get_default_values calculates default alignment parameters", {
   expect_equal(as.double(default_values["phi"]), 0.7806635, tolerance = 1e-6)
   expect_equal(as.double(default_values["psi"]), 0.8305835, tolerance = 1e-6)
 })
+
+# Define test cases for get_matrix function
+test_that("get_matrix constructs correct transformation matrix", {
+  pdb_file1 <- system.file("extdata", "1LNIA_decoy1_4.pdb",
+                           package = "TMscoreAlign"
+  )
+  pdb_file2 <- system.file("extdata", "1LNIA_decoy2_180.pdb",
+                           package = "TMscoreAlign"
+  )
+
+  alignment <- get_alignment(pdb_file1, pdb_file2,
+                             chain1 = 'A', chain2 = 'A', method = "alignment",
+                             optimize = FALSE
+  )
+
+  coord1 <- alignment$coord1
+  coord2 <- alignment$coord2
+  default_values <- get_default_values(coord1, coord2)
+  matrix <- get_matrix(default_values)
+
+  #check matrix values
+  expect_equal(matrix[1, 1], -0.5364607, tolerance = 1e-6)
+  expect_equal(matrix[1, 2], 0.4659413, tolerance = 1e-6)
+  expect_equal(matrix[1, 3], 0.7036395, tolerance = 1e-6)
+  expect_equal(matrix[1, 4], 66.96311, tolerance = 1e-6)
+
+  expect_equal(matrix[2, 1], -0.66500293, tolerance = 1e-6)
+  expect_equal(matrix[2, 2], -0.74673572, tolerance = 1e-6)
+  expect_equal(matrix[2, 3], -0.01252476, tolerance = 1e-6)
+  expect_equal(matrix[2, 4], 14.65791667, tolerance = 1e-6)
+
+  expect_equal(matrix[3, 1], 0.5195970, tolerance = 1e-6)
+  expect_equal(matrix[3, 2], -0.4746414, tolerance = 1e-6)
+  expect_equal(matrix[3, 3], 0.7104467, tolerance = 1e-6)
+  expect_equal(matrix[3, 4], -5.6408542, tolerance = 1e-6)
+
+  expect_equal(matrix[4, 1], 0)
+  expect_equal(matrix[4, 2], 0)
+  expect_equal(matrix[4, 3], 0)
+  expect_equal(matrix[4, 4], 1)
+})
